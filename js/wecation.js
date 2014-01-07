dae207653f71a812aed445832924a8893f6c1ec0
Todd Zebert <todd@toddzebert.com>
2014-01-07 15:08:53 -0800

    wrapped js in doc ready

--- js/wecation.js
+++ js/wecation.js
@@ -1,23 +1,25 @@
-$('#mainpage').live("swiperight", function() {
-    var target = $("#Plans");
-    $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                    reverse: true});
-});
-
-$('#mainpage').live("swipeleft", function() {
-    var target = $("#Cities");
-    $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                    reverse: true});
-});
-
-$('#Plans').live("swiperight", function() {
-    var target = $("#mainpage");
-    $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                    reverse: true});
-});
-
-$('#Cities').live("swipeleft", function() {
-    var target = $("#mainpage");
-    $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                    reverse: true});
+$(document).ready(function () {
+    $('#mainpage').on("swiperight", function() {
+        var target = $("#Plans");
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
+                                        reverse: true});
+    });
+    
+    $('#mainpage').on("swipeleft", function() {
+        var target = $("#Cities");
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
+                                        reverse: true});
+    });
+    
+    $('#Plans').on("swiperight", function() {
+        var target = $("#mainpage");
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
+                                        reverse: true});
+    });
+    
+    $('#Cities').on("swipeleft", function() {
+        var target = $("#mainpage");
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
+                                        reverse: true});
+    });
 });
