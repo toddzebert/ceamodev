a840a84b7a949d9120903e125135caa247466928
Todd Zebert <todd@toddzebert.com>
2014-01-07 14:44:29 -0800

    changes to swipe js and home button target added

--- js/wecation.js
+++ js/wecation.js
@@ -1,19 +1,23 @@
 $('#mainpage').live("swiperight", function() {
-    $.mobile.changePage("#Plans", {transition: "slide",
+    var target = $("#Plans");
+    $.mobile.changePage(target, {transition: "slide",
                                     reverse: true}, true, true);
 });
 
 $('#mainpage').live("swipeleft", function() {
-    $.mobile.changePage("#Cities", {transition: "slide",
+    var target = $("#Cities");
+    $.mobile.changePage(target, {transition: "slide",
                                     reverse: true}, true, true);
 });
 
 $('#Plans').live("swiperight", function() {
-    $.mobile.changePage("#mainpage", {transition: "slide",
+    var target = $("#mainpage");
+    $.mobile.changePage(target, {transition: "slide",
                                     reverse: true}, true, true);
 });
 
 $('#Cities').live("swipeleft", function() {
-    $.mobile.changePage("#mainpage", {transition: "slide",
+    var target = $("#mainpage");
+    $.mobile.changePage(target, {transition: "slide",
                                     reverse: true}, true, true);
 });
