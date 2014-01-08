e40fc84885aa682efda2d74fb24bd52b5b488efb
Todd Zebert <todd@toddzebert.com>
2014-01-07 16:00:50 -0800

    fixed status submit form and feed ids and attributes

--- js/wecation.js
+++ js/wecation.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
     $('#mainpage').on("swiperight", function() {
         var target = $("#Plans");
-        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                        reverse: true});
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide", reverse: true});
     });
     
     $('#mainpage').on("swipeleft", function() {
         var target = $("#Cities");
-        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                        reverse: true});
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide"});
     });
     
-    $('#Plans').on("swiperight", function() {
+    $('#Plans').on("swipeleft", function() {
         var target = $("#mainpage");
-        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                        reverse: true});
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide"});
     });
     
-    $('#Cities').on("swipeleft", function() {
+    $('#Cities').on("swiperight", function() {
         var target = $("#mainpage");
-        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide",
-                                        reverse: true});
+        $.mobile.pageContainer.pagecontainer("change", target, {transition: "slide", reverse: true});
     });
 });
